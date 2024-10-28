99add872b30976122e9069aab06bc3b5e09463dc
Serhii Pavlenko <md.pavlenko.serhii@gmail.com>
2024-10-28 17:48:10 +0200

    uncommited tests. deleted junk file

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
 from datetime import datetime
-from fetcher import save_to_db, BINANCE, USDT
+from fetcher import save_to_db, fetch_arp, BINANCE, USDT
 
 @pytest.mark.asyncio
 @patch('fetcher.main.get_session')
@@ -21,29 +21,29 @@ async def test_save_to_db(mock_get_session):
     mock_session.commit.assert_called_once()
 
 
-# @pytest.mark.asyncio
-# @patch('fetcher.app.main.http_request')
-# async def test_fetch_arp_success(mock_http_request):
-#     mock_response = MagicMock()
-#     mock_response.status_code = 200
-#     mock_response.json.return_value = {
-#         'data': {
-#             'savingFlexibleProduct': [{'apy': '0.0123'}]
-#         }
-#     }
-#     mock_http_request.return_value = mock_response
-#
-#     arp_value = await app.fetch_arp(app.BINANCE, app.USDT)
-#     assert arp_value == 1.23
-#
-#
-# @pytest.mark.asyncio
-# @patch('fetcher.app.main.http_request')
-# async def test_fetch_arp_invalid_data(mock_http_request):
-#     mock_response = MagicMock()
-#     mock_response.status_code = 200
-#     mock_response.json.return_value = {}
-#     mock_http_request.return_value = mock_response
-#
-#     arp_value = await app.fetch_arp(app.BINANCE, app.USDT)
-#     assert arp_value is None
+@pytest.mark.asyncio
+@patch('fetcher.main.http_request')
+async def test_fetch_arp_success(mock_http_request):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {
+        'data': {
+            'savingFlexibleProduct': [{'apy': '0.0123'}]
+        }
+    }
+    mock_http_request.return_value = mock_response
+
+    arp_value = await fetch_arp(BINANCE, USDT)
+    assert arp_value == 1.23
+
+
+@pytest.mark.asyncio
+@patch('fetcher.main.http_request')
+async def test_fetch_arp_invalid_data(mock_http_request):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {}
+    mock_http_request.return_value = mock_response
+
+    arp_value = await fetch_arp(BINANCE, USDT)
+    assert arp_value is None
